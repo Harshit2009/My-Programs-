d5b17416a545cbaf3f707df553d7b876b0761886
Harshit Goel <64546139+Harshit2009@users.noreply.github.com>
2020-06-05 11:16:52 +0530

    Add files via upload

---
+++ oops 5 series/oops5_8.py
@@ -0,0 +1,30 @@
+class Employee :
+    raise_amt = 1.04
+
+    def __init__(self,first,last,pay) :
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@company.com'
+        self.pay = pay
+        
+    def fullname(self) :
+        return '{} {}'. format(self.first, self.last)
+
+    def apply_raise(self) :
+        self.pay = int(self.pay * self.raise_amt) 
+
+    def __repr__(self) :
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+
+    def __str__(self) :
+        return '{} - {}'.format(self.fullname(), self.email)
+
+emp_1 = Employee('Harshit', 'Goel', 50000)
+emp_2 = Employee('Ajay', 'Sharma', 60000)
+
+print(1 + 2)
+
+print(int.__add__(1, 2))
+print(str.__add__('a', 'b'))
+
+        
